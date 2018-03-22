6b55313865d9516b1c36e12797463b78f7056c28
qc53bf <jesse.brand@ing.nl>
2018-03-22 10:11:30 +0100

    Mock all to reduce costs.

--- src/main/java/org/ing/hackaton/psdating/MockData.java
+++ src/main/java/org/ing/hackaton/psdating/MockData.java
@@ -40,4 +40,51 @@ public final class MockData {
         }
         return Collections.emptyList();
     }
+
+    public static final Recommendation getInfoOnCompany(final String companyName) {
+        switch(companyName) {
+        case "Praxis Rotterdam Zuid":
+            return Recommendation.builder()
+                    .companyName(companyName)
+                    .logoUrl("https://d16m3dafbknje9.cloudfront.net/imagescaler/9089876590622-424-226.jpg")
+                    .address("Stadionweg 31 31F")
+                    .zipCity("3077 AP Rotterdam")
+                    .city("Rotterdam")
+                    .phonenumber("010 - 43 22 504")
+                    .url("https://www.praxis.nl/")
+                    .build();
+        case "Hornbach Nieuwerkerk a/d IJssel":
+            return Recommendation.builder()
+                    .companyName(companyName)
+                    .logoUrl("https://www.hornbach.nl/cms/media/nl/teaser_algemeen/vestigingsinfo_1/mijn_hornbach/Drive-in-478x444_Pic_362.jpg")
+                    .address("Laan van Avant-Garde 1")
+                    .zipCity("2913LA Nieuwerkerk a/d IJssel")
+                    .city("Nieuwerkerk a/d IJssel")
+                    .phonenumber(null)
+                    .url(null)
+                    .build();
+        case "Formido Rotterdam":
+            return Recommendation.builder()
+                    .companyName(companyName)
+                    .logoUrl("https://cdn.formido.nl/default/kega_store/54550.jpg")
+                    .address(null)
+                    .zipCity(null)
+                    .city(null)
+                    .phonenumber(null)
+                    .url(null)
+                    .build();
+        case "Albert Heijn Mathenesserplein":
+            return Recommendation.builder()
+                    .companyName(companyName)
+                    .logoUrl("https://www.openingstijden.nl/assets/generated/business_logo/5688.png")
+                    .address("Mathenesserplein 76")
+                    .zipCity("3022LD Rotterdam")
+                    .city("Rotterdam")
+                    .phonenumber(null)
+                    .url(null)
+                    .build();
+        }
+        return null;
+    }
+
 }
